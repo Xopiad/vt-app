cb79479b7efbd36b189b283510147aa07e458d1e
Albert Tran <xopiad@gmail.com>
2019-08-08 23:20:32 -0400

    Add elements to profile page
    Change from column to listview
    Add profile picture, name, email
    Add method to return fields(email,etc...) to add later

--- lib/profile_page.dart
+++ lib/profile_page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'account.dart';
 
 class ProfilePage extends StatelessWidget {
-
   final Account account;
 
   ProfilePage({this.account});
@@ -15,13 +14,61 @@ class ProfilePage extends StatelessWidget {
         actions: <Widget>[],
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+        child: ListView(
+          padding: const EdgeInsets.all(8.0),
           children: <Widget>[
-            Text('Profile Page of ${account.first_name} ${account.last_name}')
+            Container(
+                width: 190.0,
+                height: 190.0,
+                margin: EdgeInsets.symmetric(vertical: 25, horizontal: 100),
+                decoration: new BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: new DecorationImage(
+                        fit: BoxFit.fill,
+                        image: new AssetImage(
+                            //TODO: Change to account.profile_picture
+                            "assets/Ving Tsun Kung Fu Association Americas.png")))),
+            Column(
+              children: <Widget>[
+                Text(
+                  '${account.first_name} ${account.last_name}',
+                  overflow: TextOverflow.ellipsis,
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+                ),
+              ],
+            ),
+            Divider(
+              color: Colors.white,
+              height: 50,
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                spacedText("email", "${account.email_address}"),
+                Divider(
+                  color: Colors.grey,
+                ),
+                spacedText("leftText", "rightText")
+              ],
+            )
           ],
         ),
       ),
+    );
+  }
+
+  Row spacedText(String leftText, String rightText) {
+    return Row(
+      children: <Widget>[
+        Text(leftText,
+            overflow: TextOverflow.ellipsis,
+            style: TextStyle(fontWeight: FontWeight.normal, fontSize: 20)),
+        Spacer(),
+        Text(rightText,
+            overflow: TextOverflow.ellipsis,
+            style: TextStyle(fontWeight: FontWeight.normal, fontSize: 20)),
+      ],
     );
   }
 }
